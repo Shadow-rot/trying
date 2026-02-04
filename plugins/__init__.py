c7950fa5a329d51b71645dcab1989ab3cf3b07b5
Shadwoo <looktouhid@gmail.com>
2026-02-04 23:33:28 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,18 @@
+"""
+Plugins Module
+Contains all bot command plugins
+"""
+
+# This file can be empty - Pyrogram will auto-discover plugins
+# But we can list available plugins here for documentation
+
+AVAILABLE_PLUGINS = [
+    'help',       # Help and start commands
+    'basic',      # Ping, alive, stats, info
+    'admin',      # Ban, mute, kick, promote, demote
+    'utilities',  # Calc, weather, translate
+    'downloader', # Download, yt, ytaudio
+    'owner',      # Restart, broadcast, shell, logs
+]
+
+__all__ = ['AVAILABLE_PLUGINS']
